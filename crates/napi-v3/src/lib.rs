73217b1c149624e5a00f88fd3480383fa1b84701
IronBlood <aplshenyang@gmail.com>
2026-01-09 17:06:40 +0800

    async

--- crates/napi-v3/src/lib.rs
+++ crates/napi-v3/src/lib.rs
@@ -4,3 +4,8 @@ use napi_derive::napi;
 pub fn hello() -> String {
     "hello from napi v3 wasm".to_string()
 }
+
+#[napi]
+pub async fn hello_async() -> String {
+    "hello from napi v3 wasm (async)".to_string()
+}
--- run-napi-v3.mjs
+++ run-napi-v3.mjs
@@ -1,3 +1,7 @@
-import { hello } from "./crates/napi-v3/hello.wasi.cjs";
+import {
+	hello,
+	helloAsync,
+} from "./crates/napi-v3/hello.wasi.cjs";
 
+helloAsync().then(x => console.log(x));
 console.log(hello());
